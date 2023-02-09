82cc0c203715608e5e67ec29a375a9c040ab0886
adiatna sukmana <dyatna.id@gmail.com>
2023-02-09 16:09:44 +0700

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,24 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            AboutSeeder::class,
-            ContactSeeder::class
+        User::create([
+            'name' => 'Bioskopy',
+            'email' => 'bioskopy@gmail.com',
+            'password' => Hash::make('123')
         ]);
     }
 }
